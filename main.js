6b6f3dba81d97e7e040c889f0c2876b376817e4b
Cesar Montelongo <cmonte905@ghotmail.com>
2019-08-03 23:11:51 -0700

    init commit:

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Hello world');
+//Ignore the web poprtion, work on the actual encrypthing and decrpyting
+//const form = document.querySelector('form');
+//
+//form.onsubmit = (_) => {
+//  const data = new FormData(form);
+//  console.log(data);
+//};
+function encrypt(value) {
+    console.log(value);
+}
+;
+function decrypt(value) {
+    console.log(value);
+}
+;
---
+++ main.ts
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+//Ignore the web poprtion, work on the actual encrypthing and decrpyting
+//const form = document.querySelector('form');
+//
+//form.onsubmit = (_) => {
+//  const data = new FormData(form);
+//  console.log(data);
+//};
+
+function encrypt(value: string){
+  console.log(value)
+};
+
+function decrypt(value: string){
+  console.log(value);
+};
