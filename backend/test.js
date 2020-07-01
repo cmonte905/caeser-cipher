51928bf176598b8dbe3e031fa81d0e8859024915
cmonte905 <cmonte905@hotmail.com>
2020-06-30 20:42:55 -0700

    Moved express app to backend folder

---
+++ backend/test.js
@@ -0,0 +1,12 @@
+const caeser = require('./cryption.js')
+
+caeser.encrypt("this is a test", 5, true)
+caeser.encrypt("ymnx nx f yjxy", 5, false)
+
+
+caeser.encrypt("this is a test", 20, true)
+caeser.encrypt("nbcm cm u nymn", 20, false)
+
+
+caeser.encrypt("this is a test", 25, true)
+caeser.encrypt("sghr hr z sdrs", 25, false)
